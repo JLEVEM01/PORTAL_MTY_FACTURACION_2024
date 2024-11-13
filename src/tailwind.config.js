b744ccbb1aad50a429996bdb55ccf021d63bd14b
JLEVEM01 <jlevem23@gmail.com>
2024-11-13 11:01:52 -0600

    commit

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -6,10 +6,8 @@ tailwind.config = {
                 clifford: '#da373d',
             },
             backgroundImage: {
-                "bgIndex": "url('/src/ssets/img/Backgrounds/bgIndex.jpg')",
-                "bgFAQ": "url('/src/assets/img/Backgrounds/bgFAQ.jpg')",
-                "bgContacto": "url('/src/assets/img/Backgrounds/bgContacto.jpg')",
-                "bgCard": "url('/src/assets/img/bgPago2.jpg')"
+                "bgFAQ": "url('/assets/img/Backgrounds/bgFAQ.jpg')",
+                "bgContacto": "url('/assets/img/Backgrounds/bgContacto.jpg')",
             },
             
         }
